c234c05b9659ee6dd78a8fb7fa1e2e8d41b4b0db
Gabriel-Bjay <jayabjay2018@gmail.com>
2025-05-15 20:11:31 +0300

    Used more examples to learn about function implementation

---
+++ function.py
@@ -0,0 +1,31 @@
+name = input("Enter your name: ")
+# Ask for two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+# Function to greet the user
+
+def greet(name):
+    print(f"Hello, {name}!")
+greet(name)
+#Function to add 2 numbers
+def add(a, b):
+    return a + b
+print("Welcome to the addition program!")
+# Call the add function and print the result
+result = add(a, b)
+print(f"Hello {name}, the sum of {a} and {b} is {result}.")
+
+def multiply(a, b):
+    return a * b
+print("Welcome to the multiplication program!")
+# # Call the multiply function and print the result
+result = multiply(a, b)
+print(f"Hello {name}, the product of {a} and {b} is {result}.")
+
+#Function to return square of a number
+def square(a):
+    return a ** 2
+print("Welcome to the square program!")
+# Call the square function and print the result
+result = square(a)
+print(f"Hello {name}, the square of {a} is {result}.")
---
+++ functions.py
@@ -0,0 +1,31 @@
+# function that adds 3 numbers
+name = input("Enter your name: ").title()
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+def add_numbers(a, b, c):
+    return a + b + c
+output = add_numbers(a, b, c)
+# Print the result
+print(f"Hello {name}, the sum of {a}, {b}, and {c} is: {output}")
+
+#function that checks if a number is even
+number = int(input("Enter a number to check if it's even: "))
+def is_even(number):
+    return number % 2 == 0
+# Check if the number is even
+if is_even(number):
+    print(f"{number} is even.")
+else:
+    print(f"{number} is odd.")
+
+
+#Function to concatenate two strings
+string1 = input("Enter your first name: ").title()
+string2 = input("Enter your last name: ").title()
+
+def full_name(string1, string2):
+    return string1 + " " + string2
+# Call the function and print the result
+result = full_name(string1, string2)
+print(f"Hello {result}, welcome to the program!")
